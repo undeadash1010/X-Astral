1f9b90edd79649900d797b93c2a042d013a40b1b
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-13 06:49:23 +0200

    🤺

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     VERSION: require('./package.json').version,
     OWNER: process.env.OWNER ||'Its You😂',
     DEEPAI_KEY: '7f831cb9-aa30-4270-bfa6-0a5d4d03afac',
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
+   //MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
     antilink: {},
     SESSION_ID: process.env.SESSION_ID || 'session_key',
