979bdbd231f7e6a11b6ad90738bcb579e1954e27
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-13 10:08:59 +0200

    🤺

--- commands/Ison.js
+++ commands/Ison.js
@@ -5,21 +5,18 @@ const { jidDecode } = require('@adiwajshing/baileys');
 Meta({
   command: 'ison',
   category: 'utility',
-  handler: async (sock, message, args) => {
-    const { key, from, isGroup } = message;
-    const { remoteJid } = key;
-
+  handler: async (sock, message, args, isGroup) => {
+    const { from } = message;
+    
     const numbers = args.input.trim().split(' ').slice(1);
     if (numbers.length === 0) {
-        return await sock.sendMessage(remoteJid, {
+        return await sock.sendMessage(from, {
         text: `*Usage:* ${config.PREFIX}ison 27686881×××`,
         quoted: message
       });
     }
-
     let res_matched = '*WhatsApp Num Ison:*\n\n';
     const results = [];
-
     for (const number of numbers) {
       const id = number.includes('@s.whatsapp.net') ? number : `${number}@s.whatsapp.net`;
       try {
@@ -27,9 +24,8 @@ Meta({
         if (status && status.length > 0) {
           const [contactInfo] = status;
           const { jid, status: about, statusTimestamp } = await sock.fetchStatus(contactInfo.jid);
-          const Date_Jid = new Date(statusTimestamp * 1000).toLocaleString();
+          const Date_Jid = new Date(statusTimestamp * 1000).toLocaleString();  
           const parsedJid = jidDecode(jid);
-
           results.push({
             jid: parsedJid.user + '@' + parsedJid.server,
             number: parsedJid.user,
@@ -37,24 +33,21 @@ Meta({
             date: statusTimestamp ? Date_Jid : '_Non_',
           });
 
-          res_matched += `*Number:* ${parsedJid.user}\n`;
-          res_matched += `*WhatsApp_JID:* ${parsedJid.user + '@' + parsedJid.server}\n`;
-          res_matched += `*About:* ${about || 'No status'}\n`;
-          res_matched += `*Status_On:* ${Date_Jid}\n`;
-          res_matched += `─────────────────────\n`;
+          res_matched += '*Number:* ' + parsedJid.user + '\n' +
+                         '*Id:* ' + parsedJid.user + '@' + parsedJid.server + '\n' +
+                         '*About:* ' + (about || 'No status') + '\n' +
+                         '*Status_On:* ' + Date_Jid + '\n' +
+                         '─────────────────────\n';
         } else {
-          res_matched += `- ${number}: Not Registered on WhatsApp\n`;
+          res_matched += '- ' + number + ': Not Registered on WhatsApp\n';
         }
       } catch (error) {
-        res_matched += `- ${number}: Error\n`;
+        res_matched += '- ' + number + ': Error\n';
       }
-    }
-
-    if (results.length > 0) {
-      res_matched += `\n*Total:* ${results.length}`;
+    } if (results.length > 0) {
+      res_matched += '\n*Total:* ' + results.length;
     } else {
       res_matched = 'No WhatsApp accounts found for the provided *num*';
-    }
-    await sock.sendMessage(remoteJid, { text: res_matched }, { quoted: message });
+    } await sock.sendMessage(from, { text: res_matched }, { quoted: message });
   }
 });
