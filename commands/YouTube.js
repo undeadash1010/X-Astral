aed30974d5f48042027b26e6c4eab232cfe58367
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-17 00:48:35 +0200

    🤺

--- commands/YouTube.js
+++ commands/YouTube.js
@@ -1,29 +1,7 @@
 const { commands, Meta } = require('../lib/');
-const { YT_SEARCH } = require('../lib/YOUTUBE.js');
 const { searchAndDownload, downloadYouTubeVideo, downloadYouTubeAudio, getYoutubeThumbnail, bytesToSize, generateId } = require('../lib/youtubei.js');
 const fs = require('fs');
 const path = require('path');
-
-Meta({
-  command: 'yts',
-  category: 'youtube',
-  handler: async (sock, message, args) => {
-    const { from } = message;
-      if (!args) {
-      return await sock.sendMessage(from, { text: 'Usage: !youtube [search query]' }, { quoted: message });
-    }
-    try {
-      const results = await YT_SEARCH(args);
-      if (results.length === 0) {
-        return await sock.sendMessage(from, { text: 'No results found' }, { quoted: message });
-      } const res = results.map((video, index) => 
-        `${index + 1}. ${video.title}\n${video.url}\n${video.description}\n*Duration*: ${video.duration}\n`
-      ).join('\n');
-      await sock.sendMessage(from, { text: `**YouTube Search:**\n\n${res}` }, { quoted: message });
-    } catch (error) {
-        }
-  }
-});
 
 Meta({
   command: 'song',
@@ -33,10 +11,8 @@ Meta({
     const query_url = args.join(' ').trim();
     if(query_url) {
     return await sock.sendMessage(from, { text: 'Provide YouTube url/song mame' });
-        }
-    const outP_kg = path.join(__dirname, 'temp_audio.mp3');
-    try {
-      if (query_url.startsWith('http')) {
+        } const outP_kg = path.join(__dirname, 'temp_audio.mp3');
+    try { if (query_url.startsWith('http')) {
         await downloadYouTubeAudio(query_url, outP_kg);
       } else {
         await searchAndDownload(query_url, outP_kg 'audio');
