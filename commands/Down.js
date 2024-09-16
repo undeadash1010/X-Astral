3e52738c12f02769d971125377274e0cb2f60860
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-16 19:39:05 +0200

    🤺

--- commands/Down.js
+++ commands/Down.js
@@ -2,6 +2,36 @@ const { commands, Meta } = require('../lib');
 const config = require('../config');
 const { IMAGE_DOWN } = require('./FUNCS_DATA/img_down.js');
 
+
+Meta({
+  command: "ringtone",
+  category: "media",
+  usage: '+ query',
+  handler: async (sock, args, message) => {
+    const { from } = message;
+    const query = args[1] || "Goku"; 
+    const fetched = async () => {
+      const res = await axios.get(`https://x-astral.apbiz.xyz/api/search/ringtone?query=${query}`);
+      return res.data;
+    };
+    fetched()
+      .then(ringtones => {
+        if (ringtones.length === 0) {
+          return sock.sendMessage(from, { text: "No_gay" });
+        } const ringtone = ringtones[0];
+        const audios = ringtone.audio;
+        return sock.sendMessage(from, {
+          audio: { url: audios },
+          mimetype: 'audio/mp3',
+          ptt: false
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+});
+  
 Meta({
   command: 'img',
   category: 'downloads',
