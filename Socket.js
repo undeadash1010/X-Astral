410cc7eb2f01083fa8ccfec9f2164b8a306415c0
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-20 14:11:19 +0200

    🤺

--- Socket.js
+++ Socket.js
@@ -1,8 +1,8 @@
 const { default: makeWASocket, 
-       useMultiFileAuthState,
-       DisconnectReason,
-       makeInMemoryStore,
-       Browsers } = require('@whiskeysockets/baileys');
+        useMultiFileAuthState, 
+        DisconnectReason, 
+        makeInMemoryStore, 
+        Browsers } = require('@whiskeysockets/baileys');
 const P = require('pino');
 const fs = require('fs');
 const axios = require('axios');
@@ -12,7 +12,7 @@ const config = require('./config');
 const { languages } = require('./data_store/languages.js');
 const { commands } = require('./lib/commands');
 const { serialised, decodeJid } = require('./lib/serialize');
-const store = makeInMemoryStore({ logger: P().child({ level: "silent", stream: "store",}),});
+const store = makeInMemoryStore({ logger: P().child({ level: "silent", stream: "store", }), });
 const SESSION_FILE = path.join(__dirname, 'auth_info_baileys', 'creds.json');
 
 async function Connect_Session() {
@@ -22,7 +22,7 @@ async function Connect_Session() {
     if (sessionId.length < 30) {
         const { data } = await axios.get(`https://pastebin.com/raw/${sessionId}`);
         sessionId = Buffer.from(data, 'base64').toString('utf8');
-    } fs.writeFileSync(SESSION_FILE, sessionId, 'utf8');
+    }  fs.writeFileSync(SESSION_FILE, sessionId, 'utf8');
 }
 
 async function startBot() {
@@ -50,21 +50,15 @@ async function startBot() {
         if (!msg.message) return;
         const sendd = msg.sender;
         const contact = store.contacts[sendd] || {};
-        const author = contact.name || sendd.split('@')[0];     
-        const messageMapping = {
-            'conversation': () => msg.text,
-            'imageMessage': () => msg.text,
-            'videoMessage': () => msg.text,
-            'extendedTextMessage': () => msg.text,
-            'buttonsResponseMessage': () => m.message.buttonsResponseMessage.selectedButtonId,
-            'listResponseMessage': () => m.message.listResponseMessage.singleSelectReply.selectedRowId,
-            'templateButtonReplyMessage': () => m.message.templateButtonReplyMessage.selectedId
-        };
-        const msgType = msg.messageType;
+        const author = contact.name || sendd.split('@')[0];
         let body = '';
-        if (messageMapping[msgType]) {
-            body = messageMapping[msgType]();
-        }          
+        if (msg.message.conversation) body = msg.message.conversation;
+        else if (msg.message.imageMessage) body = msg.message.imageMessage.caption || '';
+        else if (msg.message.videoMessage) body = msg.message.videoMessage.caption || '';
+        else if (msg.message.extendedTextMessage) body = msg.message.extendedTextMessage.text;
+        else if (msg.message.buttonsResponseMessage) body = msg.message.buttonsResponseMessage.selectedButtonId;
+        else if (msg.message.listResponseMessage) body = msg.message.listResponseMessage.singleSelectReply.selectedRowId;
+        else if (msg.message.templateButtonReplyMessage) body = msg.message.templateButtonReplyMessage.selectedId;
         const creator = config.MODS;
         const from = msg.key.remoteJid;
         const isGroup = from.endsWith('@g.us');
@@ -78,19 +72,17 @@ async function startBot() {
                         const contact = await sock.onWhatsApp(jid);
                         return contact && contact[0] && contact[0].notify ? contact[0].notify : jid.split('@')[0];
                     })
-                );      
+                );
                 const reply = (text) => {
                     sock.sendMessage(
-                        msg.key.remoteJid,  
+                        msg.key.remoteJid,
                         {
-                            text,  
-                            contextInfo: { externalAdReply: { title: "Click", body: "x-astral", mediaType: 1,  
-                            thumbnail: " ", mediaUrl: " ", sourceUrl: " ",  
-                            }}
+                            text,
+                            contextInfo: { externalAdReply: { title: "Click", body: "x-astral", mediaType: 1, thumbnail: " ", mediaUrl: " ", sourceUrl: " ", }}
                         }
                     );
-                }; 
-                
+                };
+
                 if (config.antilink) {
                     const cd_code = body.match(/https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]{10,}/g);
                     if (cd_code && !msg.key.fromMe) {
@@ -99,13 +91,13 @@ async function startBot() {
                         const groupAdmins = groupMetadata.participants
                             .filter(participant => participant.admin !== null)
                             .map(admin => admin.id);
-                        if (!groupAdmins.includes(msg.sender)) { 
+                        if (!groupAdmins.includes(msg.sender)) {
                             if (cd_code[0] !== gc_code) {
                                 const Mzg_code = `*<===Alert===>*\n\n` +
                                     `⍗ @${msg.sender.split('@')[0]}: not_allowed\n\n` +
                                     `⍗ *Link*: ${cd_code[0]}\n\n` +
                                     `⍗ *Note*: unauthorized links\n` +
-                                    `⍗  Adhere to gc_rules.`;                                   
+                                    `⍗  Adhere to gc_rules.`;
                                 await sock.sendMessage(from, { text: Mzg_code, mentions: [msg.sender] });
                                 await sock.groupParticipantsUpdate(from, [msg.sender], 'remove');
                             }
@@ -119,13 +111,13 @@ async function startBot() {
             const mode_locked = config.MODS.includes(msg.sender);
             if (config.MODE === 'private') {
                 if (!isBotAdmin && !mode_locked) return;
-            }    if (config.MODE === 'public' && command.fromMe && !isBotAdmin) {
+            }
+            if (config.MODE === 'public' && msg.key.fromMe && !isBotAdmin) {
                 return;
             } const mention_cn = msg.message.extendedTextMessage?.contextInfo?.mentionedJid?.includes(sock.user.id);
             const rep = msg.message.extendedTextMessage?.contextInfo?.stanzaId && msg.message.extendedTextMessage.contextInfo.participant === sock.user.id;
             await sock.sendMessage(from, { text: reply }, { quoted: msg });
-        }
-        if (body.startsWith(`${config.PREFIX}eval`) || body.startsWith(`${config.PREFIX}$`) ||
+        }   if (body.startsWith(`${config.PREFIX}eval`) || body.startsWith(`${config.PREFIX}$`) ||
             body.startsWith(`${config.PREFIX}>`) || body.startsWith(`${config.PREFIX}#`)) {
             const command_Type = body.charAt(config.PREFIX.length); 
             const code_Eval = body.slice(config.PREFIX.length + 2).trim();
@@ -133,22 +125,22 @@ async function startBot() {
                 await sock.sendMessage(from, { text: 'Provide_code to evaluate Example: !eval 2 + 2' });
                 return;
             }  if (msg.sender === sock.user.id || config.MODS.includes(msg.sender)) {
-                try { 
+                try {
                     const timeout = 5000;
                     let result;
                     const compile_cd = new Promise((resolve, reject) => {
-                        try { 
+                        try {
                             result = eval(code_Eval);
                             resolve(result);
                         } catch (error) {
                             reject(error);
                         }
                     });
+
                     result = await Promise.race([
                         compile_cd,
                         new Promise((_, reject) => setTimeout(() => reject(new Error('Timed out')), timeout))
-                    ]); 
-                    const output = typeof result === 'string' ? result : require('util').inspect(result);
+                    ]); const output = typeof result === 'string' ? result : require('util').inspect(result);
                     const trimmed = output.length > 2000 ? `${output.slice(0, 2000)}...` : output;
                     await sock.sendMessage(from, { text: `*OUTPUT*:\n${trimmed}` });
                 } catch (error) {
@@ -156,46 +148,26 @@ async function startBot() {
                 }
             }
         }
-        const reacts = async (emoji) => {
-            await sock.sendMessage(from, {
-                react: {
-                    text: emoji,
-                    key: msg.key 
-                }
-            });
-        };
+
         if (body.startsWith(config.PREFIX)) {
             const cmd_str = body.slice(config.PREFIX.length).trim().split(' ')[0];
             const command = commands.find(cmd => cmd.command === cmd_str);
             if (command) {
                 const args = body.slice(config.PREFIX.length + cmd_str.length).trim().split(' ');
-                try {  
+                try {
                     await command.handler({
-                        sock, msg, args, reply, isGroup, author, creator, groupMetadata, mentionedJid, mentionedJidList, groupAdmins, languages, reacts,
-                        command: cmd_str,
+                        sock, msg, args, reply, isGroup, author, creator, groupMetadata, mentionedJid, mentionedJidList, groupAdmins, languages,
                     });
-                } catch (error) {}
+                } catch (error) {
+                    console.error(error);
+                }
             }
         }
     });
-                                          
-    if (body.startsWith(`${config.PREFIX}mute`)) {
-        if (!isGroup) {
-            await sock.sendMessage(from, { text: 'This command can only be used in groups' });
-            return;
-        } const isAdmin = groupMetadata.participants.some(participant => participant.id === msg.sender && participant.admin !== null);
-        const isBotAdmin = groupMetadata.participants.some(participant => participant.id === sock.user.id && participant.admin !== null);
-        if (!isAdmin) {
-            await sock.sendMessage(from, { text: 'You must be an admin to use this command' });
-            return;
-        } if (!isBotAdmin) {
-            await sock.sendMessage(from, { text: 'I must be an admin to execute this command' });
-            return;
-        } await sock.groupSettingUpdate(from, 'announcement');
-    }
+
     sock.ev.on('group-participants.update', async (data) => {
         if (data.action === 'add') {
-            const contact = await sock.onWhatsApp(data.participants[0]);
+           const contact = await sock.onWhatsApp(data.participants[0]);
             if (!contact || !contact[0]) return;
             const contact_nemo = contact[0].notify || contact[0].jid.split('@')[0];
             const Messagei = `┌────\n` +
@@ -204,26 +176,56 @@ async function startBot() {
                 `└─────────────┘`;
             await sock.sendMessage(data.id, { text: Messagei, mentions: [data.participants[0]] });
         }
+        if (data.action === 'remove') {
+             const contact = await sock.onWhatsApp(data.participants[0]);
+            if (!contact || !contact[0]) return;
+            const contact_nemo = contact[0].notify || contact[0].jid.split('@')[0];
+            const Messagei = `┌────\n` +
+                `│ ⍗ *Goodbye* @${contact_nemo}\n` +
+                `│ ⍗ *We are excited X3*\n` +
+                `└─────────────┘`;
+            await sock.sendMessage(data.id, { text: Messagei, mentions: [data.participants[0]] });
+   
+        }
     });
-    sock.ev.on('contacts.update', (update) => {
-        update.forEach(contact => {
+
+    sock.ev.on('contacts.update', (data) => {
+        for (let contact of data) {
             const id = decodeJid(contact.id);
-            storez.contacts[id] = contact.notify;
-        });
+            if (storez.contacts[id]) storez.contacts[id].name = contact.notify;
+        }
     });
+
     sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update;
         if (connection === 'close') {
-            const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-            if (shouldReconnect) {
+            const reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+            if (reason === DisconnectReason.badSession) {
+                console.log('Bad_Session_File restart');
+                startBot();
+            } else if (reason === DisconnectReason.connectionClosed) {
+                console.log('Connection_closed, reconnecting...');
+                startBot();
+            } else if (reason === DisconnectReason.connectionLost) {
+                console.log('Connection_lost, trying to reconnect...');
+                startBot();
+            } else if (reason === DisconnectReason.loggedOut) {
+                console.log('Device_logged_out, deleting_session and trying_to_reconnect.');
+                fs.unlinkSync(SESSION_FILE);
+                startBot();
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log('Restart_required, restarting...');
+                startBot();
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log('Connection_timedOut, reconnecting...');
                 startBot();
             } else {
-                console.log('Connection closed. You are logged out');
+                console.log(`Unknown_disconnect_reason: ${reason}`);
+                startBot();
             }
         }
-        if (connection === 'open') {
-            console.log('Bot is online');
-        }
+        console.log(`Connected: ${update.connection}`);
     });
 }
+
 startBot();
