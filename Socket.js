f8d61037cbae53016e2cf2c7e8f481467da91ee9
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-24 15:20:21 +0200

    🤺

--- Socket.js
+++ Socket.js
@@ -3,20 +3,20 @@ const P = require("pino");
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
+const { PREFIX } = require('./config');
 const config = require('./config');
 const { Boom } = require('@hapi/boom');
 const { serialize, decodeJid } = require('./lib/message');
 const { commands } = require("./lib/commands");
 const { connect } = require("./lib/session");
 const store = makeInMemoryStore({ logger: P().child({ level: "silent", stream: "store" }) });
-const chalk = require("chalk"); 
 
 function loadPlugins() {
-    console.log(chalk.yellow('Installing plugins...'));
+    console.log('Installing plugins...');
     fs.readdirSync(__dirname + '/commands').forEach(plugin => {
         if (path.extname(plugin).toLowerCase() === '.js') {
-            require(__dirname + '/commands/' + plugin);
-            console.log(chalk.green(`Loaded plugin: ${plugin}`));
+            require(__dirname + '/commands/' + plugin);  
+            console.log(`Loaded plugin: ${plugin}`);
         }
     });
 }
@@ -39,79 +39,58 @@ async function startBot() {
     sock.ev.on('creds.update', saveCreds);
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update;
-
         if (connection === 'connecting') {
-            console.log(chalk.yellow('<=Connecting to WhatsAppBot...=>'));
+            console.log('<=Connecting to WhatsAppBot...=>');
         }
-
         if (connection === 'open') {
-            console.log(chalk.green('<=Login done=>'));
-            loadPlugins();
+            console.log('<=Login done=>');
+            loadPlugins();  
         }
 
         if (connection === 'close') {
             const shouldReconnect = (lastDisconnect?.error instanceof Boom) && lastDisconnect.error.output?.statusCode !== DisconnectReason.loggedOut;
-            console.log(chalk.red('Connection closed. Reconnecting...'), shouldReconnect);
-            if (shouldReconnect) startBot();
+            console.log(('Connection closed. Reconnecting...'), shouldReconnect);
+            if (shouldReconnect) startBot(); 
         }
     });
 
     sock.ev.on('messages.upsert', async (messageUpdate) => {
+        const chalk = (await import("chalk")).default;
+        const fetch = (await import("node-fetch")).default;
         const message = messageUpdate.messages[0];
         if (messageUpdate.type !== 'notify') return;
-
         const msg = serialize(message, sock);
         if (!msg.message || msg.key.remoteJid === 'status@broadcast') return;
         if (msg.type === 'protocolMessage' || msg.type === 'senderKeyDistributionMessage' || !msg.type) return;
-
         const { from, body } = msg;
         console.log(chalk.blue(`=FROM=: ${msg.isGroup ? 'Group' : 'User'}: ${from} : =CONTENT=: ${body}`));
-        const args = body.trim().split(/ +/).slice(1)
-        const text = q = args.join(" ")
-        await sock.sendMessage(from, {
-            text,
-            contextInfo: { 
-                externalAdReply: { 
-                    title: "Click", 
-                    body: "x-astral", 
-                    mediaType: 1, 
-                    thumbnail: null, 
-                    mediaUrl: null, 
-                    sourceUrl: null 
-                }
-            }
-        });
-
-        if (body.startsWith(config.PREFIX)) {
-            const cmd_str = body.slice(config.PREFIX.length).trim().split(" ")[0];
-            const command = commands.find(cmd => cmd.command === cmd_str);
-
+        const args = body.trim().split(/ +/).slice(1);
+        const text = args.join(" ");
+        const reply = (text) => {
+            sock.sendMessage(msg.from, {
+                text,
+                contextInfo: { externalAdReply: { title: "Click", body: "x-astral", mediaType: 1, thumbnail: " ", mediaUrl: " ", sourceUrl: " " } },
+            });
+        };
+        const cmdName = body.toLowerCase();
+        if (cmdName.startsWith(PREFIX)) {
+            const strippedCommand = cmdName.slice(PREFIX.length);
+            const command = commands.find(cmd => cmd.command === strippedCommand);
             if (command) {
-                const args = body
-                    .slice(config.PREFIX.length + cmd_str.length)
-                    .trim()
-                    .split(" ");
-                try {
-                    await command.handler({
-                        msg,
-                        args,
-                        reply: (text) => sock.sendMessage(from, { text }), // Assuming 'reply' means sending a message
-                        isGroup: msg.isGroup,
-                    });
+                   try {
+                    await command.handler(sock, msg, isGroup, reply); 
                 } catch (err) {
                     console.error(err);
                 }
-            }
-        }
+            }}
     });
 
     sock.ev.on('contacts.update', (update) => {
         for (let contact of update) {
             let id = decodeJid(contact.id);
             if (id && contact.notify) {
-                console.log(chalk.magenta(`Contact updated: ${id} -> ${contact.notify}`));
-            }
-            if (store && store.contacts) {
+                console.log(`Contact: ${id} -> ${contact.notify}`);
+            } if (store && store.contacts) {
                 store.contacts[id] = {
                     id,
                     name: contact.notify
